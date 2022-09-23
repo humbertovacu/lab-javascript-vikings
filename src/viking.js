c08ded5c860fe7f68ddd0a513ca68636f1149163
humbertovacu <humbertovacu@gmail.com>
2022-09-23 23:34:15 +0200

    Update on iteration #5

--- src/viking.js
+++ src/viking.js
@@ -86,11 +86,14 @@ class War {
        }
 
     showStatus(){
-        if (this.saxonArmy.length === 0) {
-            return "Vikings have won the war of the century!"
-        };
-        if (this.vikingArmy.length === 0) {
+        if(!this.saxonArmy.length){
+            return "Vikings have won the war of the century!";
+        }
+        else if(!this.vikingArmy.length){
             return "Saxons have fought for their lives and survived another day..."
+        }
+        else {
+            return "Vikings and Saxons are still in the thick of battle."
         }
        }
     
